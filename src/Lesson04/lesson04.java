4f3537942080e09ca5d41cdab87efe4e6980939b
25stapa <122109573+25stapa@users.noreply.github.com>
2023-06-29 02:30:43 +0300

    add file

---
+++ src/Lesson04/lesson04.java
@@ -0,0 +1,54 @@
+/**
+ * Реализовать консольное приложение, которое:
+ * 1. Принимает от пользователя и “запоминает” строки.
+ * 2. Если введено print, выводит строки так, чтобы последняя введенная
+ * была первой в списке, а первая - последней.
+ * 3. Если введено revert, удаляет предыдущую введенную строку из памяти.
+ * 4. Если введено exit, то программа завершается.
+ * <p>
+ * > - ввод в консоль (stdin), < - вывод на консоль (stdout)
+ * > java
+ * > python
+ * > c#
+ * > print
+ * < [c#, python, java]
+ * > revert
+ * > print
+ * < [python, java]
+ * > revert
+ * > revert
+ * > print
+ * < []
+ * > revert -> throw new NoSuchElementException
+ */
+
+package Lesson04;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class lesson04 {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value for string: ");
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("print")) {
+                for (String str : linkedList) {
+                    System.out.println(str);
+                }
+            } else if (input.equalsIgnoreCase("revert")) {
+                if (!linkedList.isEmpty()) {
+                    linkedList.pop();
+                }
+            } else if (input.equalsIgnoreCase("exit")) {
+                break;
+            } else {
+                linkedList.push(input);
+            }
+        }
+    }
+
+}
